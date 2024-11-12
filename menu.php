ed2952aa6b28ccc74afa1fddb5319ee85a61f120
Lucas07155 <lucaspraxedes74@gmail.com>
2024-11-12 20:44:06 -0300

    Initial commit

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <section class="text-center">
+            <h1>Seja bem-vindo!</h1> <br> <Br>
+            <a href="efetuarReserva.php" class="btn btn-primary">Efetuar uma reserva</a><br><br>
+            <a href="visualizarReservas.php" class="btn btn-primary">Ver reservas em aberto</a><br><br>
+        </section>
+    </div>
+</body>
+</html>
